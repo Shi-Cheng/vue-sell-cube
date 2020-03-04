a9323e029e6d05918da42eb30ae8b5f44458d919
panshicheng <x_pan2016@163.com>
2020-03-04 01:52:57 -0800

    v-header

--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -4,13 +4,6 @@
 * @date 3/3/20 12:44 AM
 * description:
 */
-
-/**
-* project: vue-sell-cube
-* @author panshicheng
-* @date ${DATE}
-* description:
-*/
 <template>
   <div class="header">
     <div class="content-wrapper">
@@ -25,14 +18,33 @@
         <div class="description">
           {{ seller.description }}/{{ seller.deliveryTime }} 分钟送达
         </div>
+        <div v-if="seller.supports" class="support">
+          <support-ico :size="1" :type="seller.supports[0].type"></support-ico>
+          <span class="text">{{ seller.supports[0].description }} </span>
+        </div>
+        <div v-if="seller.supports" class="support-count">
+          <span class="count">{{ seller.supports.length }}个</span>
+          <i class="icon-keyboard_arrow_right"/>
+        </div>
       </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"/><span class="bulletin-text">{{ seller.bulletin }}</span>
+      <i class="icon-keyboard_arrow_right"/>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
     </div>
   </div>
 </template>
 
 <script>
+import SupportIco from '../support-ico/support-ico'
 export default {
   name: 'v-header',
+  components: {
+    SupportIco
+  },
   props: {
     seller: {
       type: Object,
@@ -43,5 +55,99 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+  @import "~common/stylus/mixin.styl"
+  @import "~common/stylus/variable.styl"
+  .header
+    position: relative
+    overflow: hidden
+    color: $color-background-sssss
+    background: $color-background-ss
+    .content-wrapper
+      position: relative
+      padding: 24px 12px 18px 24px
+      font-size: $font-size-zero
+      .avatar
+        display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
+      .content
+        display: inline-block
+        margin-left: 16px
+        font-size: $font-size-large
+        .title
+          margin: 2px 0 8px 0
+          .brand
+            display: inline-block
+            vertical-align: top
+            width: 30px
+            height: 18px
+            bg-image('brand')
+            background-size: 30px 18px
+            background-repeat: no-repeat
+          .name
+            margin-left: 6px
+            font-size: 16px
+            line-height: 18px
+            font-weight: bold
+        .description
+          margin-bottom: 10px
+          line-height: 12px;
+          font-size: 12px
+        .supports
+          .text
+            vertical-align: top
+            line-height: 12px
+            font-size: 12px
+        .support-count
+          position: absolute
+          right: 12px
+          bottom: 18px
+          padding: 0 8px
+          height: 24px
+          line-height: 24px
+          border-radius: 14px
+          background: $color-background-ssss
+          .count
+            vertical-align: top
+            font-size: 10px
+          .icon-keyboard_arrow_right
+            margin-left: 4px
+            font-size: $font-size-small
+            line-height: 24px
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display : inline-block
+        vertical-align: top
+        margin-top: 7px;
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat:no-repeat
+      .bulletin-text
+        vertical-align: top
+        font-size: 10px
+        margin: 0 4px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size:10px
+        right: 12px
+        top: 8px
+    .background
+      position: absolute
+      left: 0
+      top: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 </style>
