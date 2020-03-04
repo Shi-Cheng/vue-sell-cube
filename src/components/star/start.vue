a4ed853a3e42645aa2f5c4c0fb9cbdc835d9ad93
panshicheng <x_pan2016@163.com>
2020-03-04 02:43:07 -0800

    small

---
+++ src/components/star/start.vue
@@ -0,0 +1,20 @@
+/**
+* project: vue-sell-cube
+* @author panshicheng
+* @date 3/4/20
+* description:
+*/
+
+<template>
+    <div>star</div>
+</template>
+
+<script>
+export default {
+  name: 'start'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
